fcc7ec223a9b4447abfdcd675234e23d3d8090f9
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2019-12-23 00:11:00 +0530

    Create String_Validators.py

---
+++ hackerrank/Python Language Proficiency/String_Validators.py
@@ -0,0 +1,6 @@
+str = raw_input()
+print any(c.isalnum()  for c in str)
+print any(c.isalpha() for c in str)
+print any(c.isdigit() for c in str)
+print any(c.islower() for c in str)
+print any(c.isupper() for c in str)
