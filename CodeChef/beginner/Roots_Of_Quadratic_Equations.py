24a8353698a0a3d450bee463a8b08e0ee1ea7aed
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2019-12-29 23:04:31 +0530

    Create Roots_Of_Quadratic_Equations.py
    All submissions for this problem are available.Write a program to take the values for A, B, C of a quadratic equation A∗X2+B∗X+C=0 and then find all the roots of the equation. It is guaranteed that A ≠ 0 and that the equation has at least one real root.
    
    x1=−b+(b2−4ac)√2a
    x2=−b−(b2−4ac)√2a

---
+++ CodeChef/beginner/Roots_Of_Quadratic_Equations.py
@@ -0,0 +1,10 @@
+# cook your dish here
+import math
+a = int(input())
+b = int(input())
+c = int(input())
+d = math.sqrt((b*b) - (4*a*c))
+x = (-b + d)/(2 * a)
+y = (-b - d)/(2 * a)
+print (x)
+print (y)
