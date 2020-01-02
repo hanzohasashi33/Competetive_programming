f15299defc1b7e3e5889e35e5a165c801237ae28
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-01-02 17:41:36 +0530

    Update multiply_2_big_integers.c
    Program to multiply two big numbers in C.

--- Snippets/DSA_PROBLEMS/multiply_2_big_integers.c
+++ Snippets/DSA_PROBLEMS/multiply_2_big_integers.c
@@ -2,69 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int main()
 {
     int n = 1000000;
-    char num1[n];
-    char num2[n];
-    char temp1[n];
-    char temp2[n];
-    char product[2 * n];      //maximum length of product can only be n1 + n2 ,in this case 2n
+    char num1[n],num2[n],temp1[n],temp2[n];
+    char product[2 * n];
+    int i = 0;
 
 
-    //Scanning two numbers
+    //scanning numbers as strings
     printf("Enter number 1 : ");
     scanf("%s",temp1);
     printf("Enter number 2 : ");
     scanf("%s",temp2);
 
+
     int l1 = strlen(temp1);
     int l2 = strlen(temp2);
 
-    int i = 0;
-    int j = 0;
-    int k = 0;
+    int j;
 
-    for(i = l1 - 1,j = 0;i >=0;i--,j++)
+    //loop to reverse the numbers and convert char to int
+    for(int i = l1 - 1,j = 0;i >= 0;i--,j++)
     {
         num1[j] = temp1[i] - '0';
     }
-
-    for(i = l2 -1,j = 0;i >= 0;i--,j++)
+    for(int i = l2 - 1,j = 0;i >= 0;i--,j++)
     {
-        num2[j] = temp1[i] - '0';
+        num2[j] = temp2[i] - '0';
     }
 
-    for(i = 0;i < l2;i++)
+
+
+    //loop to multiply two numbers
+    for(int i = 0;i < l2;i++)
     {
         for(j = 0;j < l1;j++)
         {
-            product[i + j] += num2[j] * num1[i];
+            product[i + j] += num2[i] * num1[j];
+            //printf("%d %d %d\n",product[i + j],i,j);
         }
     }
 
+
+    //loop to take the carry over to next digit
     int temp = 0;
-    for(i = 0;i < l1 + l2;i++)
+    for(int i = 0;i < l1 + l2;i++)
     {
-        temp = product[i]/10;
-        product[i] = product[i] % 10;
-        product[i + 1] = product[i + 1] + temp;
+      temp = product[i]/10;
+      product[i] = product[i] % 10;
+      product[i + 1] += temp;
     }
 
+
+    //loop to leave out garbage value
     for(i = l1 + l2;i >= 0;i--)
     {
-        if(product[i] > 0)
-        {
-            break;
-        }
+      if (product[i] > 0)
+      {
+        break;
+      }
     }
+  
 
-
+    //loop to print the product
     printf("Product : ");
     for(;i >= 0;i--)
     {
-        printf("%d",product[i]);
+      printf("%d",product[i]);
     }
-
-    return 0;   
+    return 0;
 }
