c3772003328129168d3d5399e172ad1ab43310e9
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-02-25 12:19:59 +0530

    Create inOrder.c

---
+++ hackerrank/Data Structures/Trees/inOrder.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+void inOrder( struct node *root) 
+{
+    if(root == NULL)
+    {
+        return ;
+    }
+
+    inOrder(root->left);
+    printf("%d ",root->data);
+    inOrder(root->right);
+}
+
