0e92e6fb41d3efbf44e0c99adff909eb87daea5e
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-02-25 13:10:07 +0530

    Create height_of_tree.c
    find height of binary tree(height - 1)
    to find height return 0;

---
+++ hackerrank/Data Structures/Trees/height_of_tree.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+int getHeight(struct node* root) {
+    // Write your code here
+    if(root == NULL)
+        return -1;
+    
+    else 
+    {
+        int ldepth = getHeight(root->left);
+        int rdepth = getHeight(root->right);
+
+        if(ldepth > rdepth)
+        {
+            return ldepth + 1;
+        }
+        else 
+        {
+            return rdepth + 1;
+        }
+    }
+}
+
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  
+	printf("%d",getHeight(root));
+    return 0;
+}
