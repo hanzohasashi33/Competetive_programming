2bd864c0398ca704ce78712889d923931bd91a84
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-03-16 17:13:22 +0530

    Create Reverse_integer.py
    Given a 32-bit signed integer, reverse digits of an integer.

---
+++ LeetCode/Reverse_Integer/Reverse_integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 2**31 - 1 or x < -2**31 :
+            return 0
+        else :
+            reversed = 0
+            if x > 0 :
+                while x > 0 :
+                    reversed *= 10
+                    reversed += int(x%10)
+                    x = int(x/10)
+                if reversed > 2**31 -1 :
+                    return 0
+                else :
+                    return reversed
+            else :
+                x = -x
+                while x > 0 :
+                    reversed *= 10
+                    reversed += int(x%10)
+                    x = int(x/10)
+                if -reversed < -2**31 :
+                    return 0
+                else :
+                    return -reversed
+            
+            
+        
