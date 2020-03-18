abe47fdcecc4e34565ed0bf80d2c835fa6876a71
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2020-03-18 19:02:13 +0530

    Create Level_Order_Traversal.cpp

---
+++ hackerrank/Data Structures/Trees/Level_Order_Traversal.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) 
+    {
+        queue<Node *> q;
+        Node *temp=root;
+        if(root != NULL)
+        {
+            q.push(root);
+        }
+        while(!q.empty())
+        {
+            cout<<temp->data<<" ";
+            if(temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+         
+            q.pop();
+            temp= q.front();
+        
+        }
+
+    }
+
+}; //End of Solution
