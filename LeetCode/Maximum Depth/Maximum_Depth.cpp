a0796b95c8a06f52fccc3a7b5833d55c56efef09
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-03-21 19:33:08 +0530

    Create Maximum_Depth.cpp

---
+++ LeetCode/Maximum Depth/Maximum_Depth.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        else return max(maxDepth(root->left) + 1,maxDepth(root->right) + 1);
+    }
+};
