e952ab8fd83581d725b55b975cd0394826753fe4
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-03-25 17:03:26 +0530

    Create Insert_Node_In_BST.cpp
    Given a BST and some keys, you need to insert the keys in the given BST. Duplicates are not inserted (If a test case contains duplicate keys, you need to consider the first occurrence and ignore duplicates).

---
+++ Geeks4Geeks/BST/Insert_Node_In_BST.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+   int data;
+   Node* left;
+   Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+   Node* temp = new Node;
+   temp->data = val;
+   temp->left = NULL;
+   temp->right = NULL;
+
+   return temp;
+}
+
+
+
+Node* insert(Node* node, int data);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = newNode(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = newNode(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = newNode(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+       getline(cin, s);
+        int k = stoi(s);
+       // getline(cin, s);
+
+        insert(root, k);
+        vector<int> v;
+        inorder(root, v);
+        for(int i=0;i<v.size();i++)
+            cout << v[i] << " ";
+        cout << endl;
+
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
+/* The structure of a BST node is as follows:
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+
+}; 
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+   Node* temp = new Node;
+   temp->data = val;
+   temp->left = NULL;
+   temp->right = NULL;
+
+   return temp;
+}
+
+
+*/
+
+/* This function should insert a new node with 
+  given data and return root of the modified tree  */
+Node* insert(Node* node, int data)
+{
+    // Your code here
+    if(node == NULL) return newNode(data);
+    
+    
+    
+    if(data < node->data)
+        node->left = insert(node->left,data);
+    else if(data > node->data)
+        node->right = insert(node->right,data);
+    
+    return node;
+    
+}
