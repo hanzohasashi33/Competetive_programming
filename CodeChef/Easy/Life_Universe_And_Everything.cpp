13832fdac659efc7562b11254fcbc2ce3135bb38
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-03-30 19:16:22 +0530

    Create Life_Universe_And_Everything.cpp

---
+++ CodeChef/Easy/Life_Universe_And_Everything.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  int num;
+  cin >> num;
+  while(num != 42) 
+  {
+    cout << num << endl;
+    cin >> num;
+  }
+  return 0;
+}
