e2c0fd7d51f45130f453a8573105888c8ed5fa8f
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-04-01 12:31:50 +0530

    Create Flipping_bits.cpp

---
+++ hackerrank/problem_solving/Flipping_bits.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the flippingBits function below.
+long flippingBits(long n) 
+{
+    return 4294967295 - n;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
