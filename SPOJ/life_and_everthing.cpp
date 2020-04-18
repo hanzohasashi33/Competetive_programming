3f9a02513212af666440ee9079007e6ea10ed563
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-04-18 12:14:49 +0530

    Create life_and_everthing.cpp

---
+++ SPOJ/life_and_everthing.cpp
@@ -0,0 +1,17 @@
+//https://www.spoj.com/problems/TEST/
+//May the force be with you.
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    while(true)
+    {
+        int num;
+        cin >> num;
+        if(num == 42) break;
+        cout << num << endl;
+    }
+}
