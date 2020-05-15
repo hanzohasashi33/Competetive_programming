e3231462daacdae1df51fb6b2fae1d5b3bb51f02
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-05-15 09:44:55 +0530

    Create Day18.py

---
+++ hackerrank/30 Days of Code/Day18.py
@@ -0,0 +1,48 @@
+import sys
+
+class Solution:
+    # Write your code here
+    #def __init__(self,s,q) :
+    s = []
+    q = [] 
+    
+    def pushCharacter(self,ch) :
+        self.s.append(ch)
+    
+    def enqueueCharacter(self,ch) :
+        self.q.append(ch)
+    
+    def popCharacter(self) :
+        x = self.s.pop()
+        return x 
+    
+    def dequeueCharacter(self) :
+        x = self.q.pop(0)
+        return x
+
+# read the string s
+s=input()
+#Create the Solution class object
+obj=Solution()   
+
+l=len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+''' 
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+#finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")    
