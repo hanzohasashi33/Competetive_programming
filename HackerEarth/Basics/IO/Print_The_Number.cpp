7f2f7dee140922fe027f0cbce642f0349a748e91
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-07-28 19:16:31 +0530

    Create Print_The_Number.cpp

---
+++ HackerEarth/Basics/IO/Print_The_Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Write your code here
+int main()
+{
+	int n;
+	cin >> n;
+	int temp;
+	for(int i = 0;i < n;++i)
+	{
+		cin >> temp;
+		cout << temp << " ";
+	}
+	return 0;
+}
