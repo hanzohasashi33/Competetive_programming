4412e72c51e3d363fb261da021f1d49dfdce9436
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-07-29 17:41:31 +0530

    Create Hello.java

---
+++ HackerEarth/Basics/IO/Hello.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hello Kirti");  
+
+        // Write your code here
+
+    }
+}
