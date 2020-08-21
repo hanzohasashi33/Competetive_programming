326ec05eba16ff547f9c4c670967c47c77e9fe58
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-08-21 17:24:34 +0530

    Create ifElse.java

---
+++ hackerrank/Java Language Proficiency/ifElse.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String message = "";
+        if(n%2 != 0)
+        {
+            message = "Weird";
+        }
+        else if(n > 2 && n <5)
+        {
+            message = "Not Weird";
+        }
+        else if(n > 6 && n <= 20)
+        {
+            message = "Weird";
+        }
+        else if(n > 20)
+        {
+            message = "Not Weird";
+        }
+        System.out.println(message);
+        scanner.close();
+    }
+}
