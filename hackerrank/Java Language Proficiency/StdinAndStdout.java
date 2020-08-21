af588c268ddb4c800f7dc12958ca13c5c03fe92f
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-08-21 17:20:19 +0530

    Create StdinAndStdout.java

---
+++ hackerrank/Java Language Proficiency/StdinAndStdout.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
