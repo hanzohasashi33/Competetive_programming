2c7f685cbf9351d462a8f8cc1b452fcf93c88814
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-30 20:10:04 +0530

    encryption

---
+++ hackerrank/problem_solving/encryption.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the encryption function below.
+string encryption(string s) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = encryption(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
