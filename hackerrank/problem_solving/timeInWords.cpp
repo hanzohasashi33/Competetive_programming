cb830c757cf9091fdba1d5076c0ca79b568ac0fd
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-30 20:07:49 +0530

    Time in words

---
+++ hackerrank/problem_solving/timeInWords.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the timeInWords function below.
+string timeInWords(int h, int m) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int h;
+    cin >> h;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int m;
+    cin >> m;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result = timeInWords(h, m);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
