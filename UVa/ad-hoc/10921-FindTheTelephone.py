33e5af898c1ee09e435af1f58e71f8a6ebd2c984
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-12-18 22:36:49 +0530

    Create 10921-FindTheTelephone.py

---
+++ UVa/ad-hoc/10921-FindTheTelephone.py
@@ -0,0 +1,46 @@
+if __name__ == "__main__":
+    dict = {
+            "A":2,
+            "B":2,
+            "C":2,
+            "D":3,
+            "E":3,
+            "F":3,
+            "G":4,
+            "H":4,
+            "I":4,
+            "J":5,
+            "K":5,
+            "L":5,
+            "M":6,
+            "N":6,
+            "O":6,
+            "P":7,
+            "Q":7,
+            "R":7,
+            "S":7,
+            "T":8,
+            "U":8,
+            "V":8,
+            "W":9,
+            "X":9,
+            "Y":9,
+            "Z":9
+    }
+    
+    
+    while True:
+        try:
+            n = input()
+            output = ""
+            for i in n:
+                if i in dict:
+                    output += str(dict[i])
+                else:
+                    output += str(i) 
+
+            print(output)
+
+
+        except EOFError:
+            break
